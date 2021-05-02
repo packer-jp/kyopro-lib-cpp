b5b7601be209589556f499b0c0f714d1706511aa
packer_jp <tone0805develop@gmail.com>
2021-05-02 21:05:03 +0900

    テンプレートの更新

---
+++ template.hpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define all(a) (a).begin(), (a).end()
+#define bit(n) (1LL << (n))
+using ll = long long;
+template <typename T> using priority_queue_rev = priority_queue<T, vector<T>, greater<T>>;
