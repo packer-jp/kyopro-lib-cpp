ef10fcadb27d2444db7c036f5ed4a1587b5c9ec0
packer_jp <tone0805develop@gmail.com>
2021-09-12 03:49:23 +0900

    update

--- test/judge.yosupo.jp/Bitwise_And_Convolution.1.test.cpp
+++ test/judge.yosupo.jp/Bitwise_And_Convolution.1.test.cpp
@@ -11,12 +11,16 @@ int main() {
     vector<mint> a(bit(n)), b(bit(n));
     for (ll i : rep(bit(n))) cin >> a[i];
     for (ll i : rep(bit(n))) cin >> b[i];
-    for (ll i : rep(bit(n - 1))) {
-        swap(a[i], a[i ^ ((bit(n) - 1))]);
-        swap(b[i], b[i ^ ((bit(n) - 1))]);
+    if (n > 0) {
+        for (ll i : rep(bit(n - 1))) {
+            swap(a[i], a[i ^ ((bit(n) - 1))]);
+            swap(b[i], b[i ^ ((bit(n) - 1))]);
+        }
     }
     vector<mint> c = or_convolution(a, b);
-    for (ll i : rep(bit(n - 1))) swap(c[i], c[i ^ ((bit(n) - 1))]);
+    if (n > 0) {
+        for (ll i : rep(bit(n - 1))) swap(c[i], c[i ^ ((bit(n) - 1))]);
+    }
     for (mint ci : c) { cout << ci << " "; }
     cout << endl;
 }
