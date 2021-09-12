e3219941e0443c4ea8d85b48abebd61a55e59e9c
packer_jp <tone0805develop@gmail.com>
2021-09-12 19:48:10 +0900

    update

--- graph/dinic.hpp
+++ graph/dinic.hpp
@@ -3,15 +3,15 @@
 #include "../template.hpp"
 
 template <typename S> struct dinic {
-    using cap_t = typename S::cap_t;
+    using C = typename S::cost_t;
     struct edge {
         int to, rev;
-        cap_t cap;
+        C cap;
         bool is_rev;
     };
     vector<vector<edge>> graph;
     vector<int> level, iter;
-    void add_edge(int from, int to, cap_t cap) {
+    void add_edge(int from, int to, C cap) {
         graph[from].push_back({to, (int)graph[to].size(), cap, false});
         graph[to].push_back({from, (int)graph[from].size() - 1, S::zero(), true});
     }
@@ -31,12 +31,12 @@ template <typename S> struct dinic {
             }
         }
     }
-    cap_t dfs(int v, int t, cap_t f) {
+    C dfs(int v, int t, C f) {
         if (v == t) return f;
-        for (int i : rep(iter[v], graph[v].size())) {
+        for (int &i = iter[v]; i < graph[v].size(); ++i) {
             edge &e = graph[v][i];
             if (level[v] >= level[e.to] || e.cap == S::zero()) continue;
-            cap_t d = dfs(e.to, t, f > e.cap ? e.cap : f);
+            C d = dfs(e.to, t, f > e.cap ? e.cap : f);
             if (d == S::zero()) continue;
             e.cap -= d;
             graph[e.to][e.rev].cap += d;
@@ -44,22 +44,22 @@ template <typename S> struct dinic {
         }
         return S::zero();
     }
-    cap_t calc_max_flow(int s, int t, cap_t lim) {
-        cap_t ret = S::zero();
+    C calc_max_flow(int s, int t, C lim) {
+        C ret = S::zero();
         while (true) {
             bfs(s);
             if (level[t] < 0 || lim == S::zero()) { return ret; }
             fill(iter.begin(), iter.end(), 0);
-            cap_t f;
+            C f;
             while ((f = dfs(s, t, lim)) != S::zero()) {
                 ret += f;
                 lim -= f;
             }
         }
     }
-    cap_t calc_max_flow(int s, int t) { return calc_max_flow(s, t, S::inf()); }
-    map<pair<int, int>, cap_t> get_max_flow() {
-        map<pair<int, int>, cap_t> ret;
+    C calc_max_flow(int s, int t) { return calc_max_flow(s, t, S::inf()); }
+    map<pair<int, int>, C> get_max_flow() {
+        map<pair<int, int>, C> ret;
         for (int i : rep(graph.size())) {
             for (edge &e : graph[i]) {
                 if (e.is_rev) ret[{e.to, i}] = e.cap;
@@ -67,8 +67,8 @@ template <typename S> struct dinic {
         }
         return ret;
     }
-    map<pair<int, int>, cap_t> get_min_cut(int s) {
-        map<pair<int, int>, cap_t> ret;
+    map<pair<int, int>, C> get_min_cut(int s) {
+        map<pair<int, int>, C> ret;
         vector<int> visited(graph.size());
         queue<int> que;
         que.push(s);
@@ -92,7 +92,7 @@ template <typename S> struct dinic {
 };
 
 struct ll_dinic {
-    using cap_t = ll;
-    static cap_t zero() { return 0; }
-    static cap_t inf() { return numeric_limits<cap_t>::max(); }
+    using cost_t = ll;
+    static cost_t zero() { return 0; }
+    static cost_t inf() { return numeric_limits<cost_t>::max(); }
 };
