a3ecb2f08f5c8dd81be186800ecaa3ffd5fbb784
packer_jp <tone0805develop@gmail.com>
2021-09-17 13:39:29 +0900

    update

--- string/rolling_hash.hpp
+++ string/rolling_hash.hpp
@@ -5,13 +5,13 @@
 
 struct rolling_hash {
     static constexpr ull MOD = bit(61) - 1;
-    static vector<ull> pb;
+    static vector<ull> pbase;
     vector<ull> hash;
-    static void resize_pb(int n) {
-        int sz = pb.size();
+    static void resize_pbase(int n) {
+        int sz = pbase.size();
         if (sz > n) return;
-        pb.resize(n + 1);
-        for (int i : rep(sz - 1, n)) pb[i + 1] = mul(pb[i], pb[1]);
+        pbase.resize(n + 1);
+        for (int i : rep(sz - 1, n)) pbase[i + 1] = mul(pbase[i], pbase[1]);
     }
     template <typename T> static T calc_mod(T val) {
         val = (val & MOD) + (val >> 61);
@@ -20,17 +20,17 @@ struct rolling_hash {
     }
     static ull mul(ull a, ull b) { return calc_mod((__uint128_t)a * b); }
     static ull concat(ull lhs, ull rhs, int rn) {
-        resize_pb(rn);
-        return calc_mod(mul(lhs, pb[rn]) + rhs);
+        resize_pbase(rn);
+        return calc_mod(mul(lhs, pbase[rn]) + rhs);
     }
     rolling_hash(const string &src) : hash(src.size() + 1) {
-        for (int i : rep(src.size())) hash[i + 1] = calc_mod(mul(hash[i], pb[1]) + src[i]);
-        resize_pb(src.size());
+        for (int i : rep(src.size())) hash[i + 1] = calc_mod(mul(hash[i], pbase[1]) + src[i]);
+        resize_pbase(src.size());
     }
     template <typename T> rolling_hash(const vector<T> &src) : hash(src.size() + 1) {
-        for (int i : rep(src.size())) hash[i + 1] = calc_mod(mul(hash[i], pb[1]) + src[i]);
-        resize_pb(src.size());
+        for (int i : rep(src.size())) hash[i + 1] = calc_mod(mul(hash[i], pbase[1]) + src[i]);
+        resize_pbase(src.size());
     }
-    ull get_hash(int l, int r) const { return calc_mod(MOD - mul(hash[l], pb[r - l]) + hash[r]); }
+    ull get_hash(int l, int r) const { return calc_mod(MOD - mul(hash[l], pbase[r - l]) + hash[r]); }
 };
-vector<ull> rolling_hash::pb{1, xor64(MOD >> 1, MOD)};
+vector<ull> rolling_hash::pbase{1, xor64(MOD >> 1, MOD)};
