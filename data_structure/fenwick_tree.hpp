cd93b2b899939e5e72205e7eed1c96f3c0d83838
packer_jp <tone0805develop@gmail.com>
2021-09-30 10:57:22 +0900

    update

---
+++ data_structure/fenwick_tree.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "../template.hpp"
+
+template <typename T> struct fenwick_tree {
+    vector<T> data;
+    fenwick_tree(int n) : data(n + 1, T()) {}
+    void add(int i, const T &x) {
+        for (++i; i <= data.size(); i += i & -i) data[i] += x;
+    }
+    T sum(int i) const {
+        T ret = T();
+        for (; i > 0; i -= i & -i) { ret += data[i]; }
+        return ret;
+    }
+    T sum(int l, int r) const { return sum(r) - sum(l); }
+};
---
+++ test/onlinejudge.u-aizu.ac.jp/Range_Sum_Query.0.test.cpp
@@ -0,0 +1,20 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/3/DSL/2/DSL_2_H"
+#include "../../data_structure/fenwick_tree.hpp"
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ll n, q;
+    cin >> n >> q;
+    fenwick_tree<ll> ft(n);
+    while (q--) {
+        ll com, x, y;
+        cin >> com >> x >> y, --x;
+        if (com == 0) {
+            ft.add(x, y);
+        } else if (com == 1) {
+            cout << ft.sum(x, y) << endl;
+        }
+    }
+}
